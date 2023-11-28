6dad9c8a7a88ecec6d95d36293eab0c6aa7eaee9
gabriele-kaskeviciute <gabi.kaskeviciute@gmail.com>
2023-11-28 18:38:34 +0200

    JS-9 DOM 2

---
+++ scrip.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+   
+button.style.position = "absolute"
+button.style.right = "0%"
+button.style.bottom = "0%"
+document.body.style.background = "#EAAFCC"
+
+
+
+})
